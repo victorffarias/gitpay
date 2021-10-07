bf6c5b660c2b429d646a3284df68f88fbd038307
Victor Farias <victor.f.farias@gmail.com>
2021-10-06 22:44:11 -0300

    [ADD] Adding labels and form for login page

--- frontend/src/components/session/login-page.js
+++ frontend/src/components/session/login-page.js
@@ -1,9 +1,14 @@
-import { styled, Card } from '@material-ui/core'
+import { styled, Card, Typography } from '@material-ui/core'
+import { withStyles } from '@material-ui/styles';
 import React, { Component } from 'react'
+import { FormattedMessage } from 'react-intl';
 import { withRouter } from 'react-router'
+import LoginForm from '../../containers/login-form';
+
+import styles from '../styles/style'
 
 const backgroundImage = require('../../images/login-background.jpg');
-const logoGitpay = require('../../images/logo-complete.png')
+const logoGitpay = require('../../images/logo-without-symbol.png')
 
 const PageWrapper = styled('div')({
     display: 'flex',
@@ -26,27 +31,46 @@ const FormWrapper = styled(Card)({
     justifyContent: 'top',
     alignItems: 'center',
 
-    padding: '30px',
-    width: '550px',
-    height: '875px',
+    padding: '60px',
+    width: '450px',
+    height: '775px',
 })
 
 const GitpayLogo = styled('img')({
-    width: '80%',
-    objectFit: 'scale-down'
-    
+    width: '60%',
+    objectFit: 'scale-down',
+    marginBottom: '20px'
 })
+
+const HeadingTypography = styled(Typography)(({theme}) => ({
+    fontWeight: 700,
+    fontSize: '2rem',
+    color: theme.palette.text.primary 
+}))
+
+const SubheadingTypography = styled(Typography) (({ theme }) => ({
+    fontWeight: 450,
+    fontSize: '1.2rem',
+    color: theme.palette.text.secondary
+}))
 
 class LoginPage extends Component {
     render() {
         return (
             <PageWrapper>
-                <FormWrapper>
+                <FormWrapper variant="elevation">
                     <GitpayLogo src={logoGitpay} />
+                    <HeadingTypography variant="h1" noWrap>
+                        <FormattedMessage id="account.register.create.headline" defaultMessage="Welcome!" />
+                    </HeadingTypography>
+                    <SubheadingTypography variant="h2" gutterBottom noWrap>
+                        <FormattedMessage id="account.register.create.subheadline" defaultMessage="Create your account" />
+                    </SubheadingTypography>
+                    <LoginForm />
                 </FormWrapper>
             </PageWrapper>
         )
     }
 }
 
-export default withRouter(LoginPage)
+export default withRouter(withStyles(styles)(LoginPage))
